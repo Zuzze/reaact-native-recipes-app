0d284fdebe1678acc3444af93dbcf8876f23bcae
Zuzze <nevalsus@gmail.com>
2020-11-13 16:21:41 +0100

    category grid added with platform specific style

---
+++ constants/theme.js
@@ -0,0 +1,28 @@
+const waterMelonSugarTheme = {
+  text: "gray",
+  primary: "#ff7676",
+  accent: "#e7ffea",
+  danger: "#ffcdc8",
+  success: "#e7ffea",
+  shadow: "gray",
+
+  header: "#ff7676",
+  headerText: "white",
+  background: "#e7ffea",
+  input: "white",
+  card: "white",
+  cardBorderRadius: 20,
+  button: "#bc8f7d",
+  buttonText: "white",
+  buttonBorderRadius: 20,
+  buttonRipple: "lightgray",
+
+  titleFontFamily: "open-sans-bold",
+  fontFamily: "open-sans-regular",
+  titleFontColor: "gray"
+};
+
+// selected theme
+const Theme = waterMelonSugarTheme;
+
+export default Theme;
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,22 +1,45 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  FlatList,
+  TouchableOpacity
+} from "react-native";
 import PropTypes from "prop-types";
+import { CATEGORIES } from "../data/mock-data";
+import Theme from "../constants/theme";
+import { Platform } from "@unimodules/core";
 
 /**
  * Screen to display recipe categories
  * @param {*} props
  */
 const CategoriesScreen = props => {
+  const renderGridItem = itemData => {
+    return (
+      <TouchableOpacity style={styles.gridItem}>
+        <View>
+          <Text>{itemData.item.title}</Text>
+          <Button
+            title="Show"
+            onPress={() => {
+              props.navigation.navigate({ routeName: "Recipes" });
+            }}
+          />
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
-    <View style={styles.screen}>
-      <Text>Categories</Text>
-      <Button
-        title="Recipes"
-        onPress={() => {
-          props.navigation.navigate({ routeName: "Recipes" });
-        }}
-      />
-    </View>
+    <FlatList
+      keyExtractor={item => item.id}
+      numColumns={2}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+    />
   );
 };
 
@@ -24,11 +47,25 @@ CategoriesScreen.propTypes = {};
 
 CategoriesScreen.defaultProps = {};
 
+CategoriesScreen.navigationOptions = {
+  // see header style documentation at https://reactnavigation.org/docs/headers/
+  headerTitle: "Categories",
+  headerStyle: {
+    backgroundColor: Platform.OS === "android" ? Theme.header : Theme.background
+  },
+  headerTintColor: Theme.headerText
+};
+
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  gridItem: {
+    margin: 15,
+    flex: 1,
+    height: 150
   }
 });
 
