36e3d01a25c67fda366f5f6dba6878219f2f885b
Zuzze <nevalsus@gmail.com>
2020-11-15 16:08:26 +0100

    toggle favorite logic to redux

--- screens/ItemScreen.js
+++ screens/ItemScreen.js
@@ -1,12 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
 import { View, Text, StyleSheet, ScrollView, Image } from "react-native";
 import PropTypes from "prop-types";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Theme from "../constants/theme";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../components/HeaderButton";
 import ThemeTitleText from "../components/ThemeTitleText";
 import ThemeText from "../components/ThemeText";
+import { toggleFavorite } from "../store/actions/items";
 
 const ListItem = props => {
   return (
@@ -22,12 +23,21 @@ const ListItem = props => {
  */
 const ItemScreen = props => {
   const availableItems = useSelector(state => state.items.items);
-  const ItemId = props.navigation.getParam("id");
-  const selectedItem = availableItems.find(item => item.id === ItemId);
+  const itemId = props.navigation.getParam("id");
+  const selectedItem = availableItems.find(item => item.id === itemId);
+  const dispatch = useDispatch();
+
+  const handleToggleFavorite = useCallback(() => {
+    dispatch(toggleFavorite(itemId));
+  }, [dispatch, itemId]);
 
   useEffect(() => {
     props.navigation.setParams({ itemTitle: selectedItem.title });
   }, [selectedItem]);
+
+  useEffect(() => {
+    props.navigation.setParams({ toggleFavorite: handleToggleFavorite });
+  }, [handleToggleFavorite]);
 
   return (
     <View style={styles.screen}>
@@ -53,18 +63,14 @@ const ItemScreen = props => {
   );
 };
 ItemScreen.navigationOptions = navigationData => {
-  const itemId = navigationData.navigation.getParam("id");
   const itemTitle = navigationData.navigation.getParam("itemTitle");
+  const toggleFavorite = navigationData.navigation.getParam("toggleFavorite");
 
   return {
     headerTitle: itemTitle,
     headerRight: () => (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
-        <Item
-          title="Favorite"
-          iconName="ios-star"
-          onPress={() => console.log("fav")}
-        />
+        <Item title="Favorite" iconName="ios-star" onPress={toggleFavorite} />
       </HeaderButtons>
     )
   };
--- store/reducers/items.js
+++ store/reducers/items.js
@@ -1,4 +1,5 @@
 import { ITEMS } from "../../data/mock-data";
+import { TOGGLE_FAVORITE } from "../actions/items";
 
 const initialState = {
   items: ITEMS,
@@ -7,7 +8,27 @@ const initialState = {
 };
 
 const itemsReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case TOGGLE_FAVORITE:
+      const existingIndex = state.favoriteItems.findIndex(
+        item => item.id === action.id
+      );
+      if (existingIndex >= 0) {
+        // was already favorite, remove item from favorites
+        const updatedFavorites = [...state.favoriteItems];
+        updatedFavorites.splice(existingIndex, 1);
+        return { ...state, favoriteItems: updatedFavorites };
+      } else {
+        // was not yet favorite, add favorite
+        const itemToAdd = state.items.find(item => item.id === action.id);
+        return {
+          ...state,
+          favoriteItems: state.favoriteItems.concat(itemToAdd)
+        };
+      }
+    default:
+      return state;
+  }
 };
 
 export default itemsReducer;
